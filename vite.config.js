2495e7536c51a038e6389882b86e4c2389fb9a2b
Anish-boraniya <anishboraniya2004@gmail.com>
2025-05-09 14:02:04 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  base: "/",
   build: {
-    // Output directory
-    outDir: "dist",
-    // Generate source maps for production (can be set to false for smaller builds)
+    outDir: 'dist',
     sourcemap: false,
-    // Configure rollup options if needed
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
+    minify: true,
   },
-});
+})
